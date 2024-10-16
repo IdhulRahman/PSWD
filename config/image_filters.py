8d1c9801afa46671a4416b6684aa675a076ce4a2
IdhulRahman <idhulrahim@gmail.com>
2024-10-16 10:11:11 +0700

    Update image_filters.py

--- config/image_filters.py
+++ config/image_filters.py
@@ -17,5 +17,7 @@ def upload_image():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "bmp"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
         return np.array(image)
+    st.warning("Please upload an image file.")
     return None
