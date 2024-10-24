f6b1621b6b29934be4aa2afba1f6f07718f8a696
IdhulRahman <idhulrahim@gmail.com>
2024-10-25 06:08:29 +0700

    Update mesh.py

--- mesh.py
+++ mesh.py
@@ -1,20 +1,26 @@
 from mininet.topo import Topo
 
-class MyMeshTopo( Topo ):
-    "Full mesh topology example."
+class MyTopo( Topo ):
+    "Simple topology example."
 
     def build( self ):
         "Create custom topo."
 
         # Add hosts
-        h1 = self.addHost( 'h1' )
-        h2 = self.addHost( 'h2' )
-        h3 = self.addHost( 'h3' )
-        h4 = self.addHost( 'h4' )
-        h5 = self.addHost( 'h5' )
-        h6 = self.addHost( 'h6' )
-        
-        # Add switches
+        h1 = self.addHost( 'h1', ip='20.20.20.1' )
+        h2 = self.addHost( 'h2', ip='20.20.20.2' )
+        h3 = self.addHost( 'h3', ip='20.20.20.3' )
+        h4 = self.addHost( 'h4', ip='20.20.20.4' )
+        h5 = self.addHost( 'h5', ip='20.20.20.5' )
+        h6 = self.addHost( 'h6', ip='20.20.20.6' )
+        h7 = self.addHost( 'h7', ip='20.20.20.7' )
+        h8 = self.addHost( 'h8', ip='20.20.20.8' )
+        h9 = self.addHost( 'h9', ip='20.20.20.9' )
+        h10 = self.addHost( 'h10', ip='20.20.20.10' )
+        h11 = self.addHost( 'h11', ip='20.20.20.11' )
+        h12 = self.addHost( 'h12', ip='20.20.20.12' )
+
+        # Add switch
         s1 = self.addSwitch( 's1' )
         s2 = self.addSwitch( 's2' )
         s3 = self.addSwitch( 's3' )
@@ -22,18 +28,25 @@ class MyMeshTopo( Topo ):
         s5 = self.addSwitch( 's5' )
         s6 = self.addSwitch( 's6' )
         
-        # Add links to form a full mesh
-        switches = [s1, s2, s3, s4, s5, s6]
-        for i in range(len(switches)):
-            for j in range(i + 1, len(switches)):
-                self.addLink(switches[i], switches[j])
-
-        # Add host links to switches
+        # Add links switch
+        self.addLink( s1, s2 )
+        self.addLink( s2, s3 )
+        self.addLink( s3, s4 )
+        self.addLink( s5, s6 )
+        self.addLink( s6, s1 )
+        
+        # Add links host
         self.addLink( s1, h1 )
-        self.addLink( s2, h2 )
-        self.addLink( s3, h3 )
-        self.addLink( s4, h4 )
-        self.addLink( s5, h5 )
-        self.addLink( s6, h6 )
+        self.addLink( s1, h2 )
+        self.addLink( s2, h3 )
+        self.addLink( s2, h4 )
+        self.addLink( s3, h5 )
+        self.addLink( s3, h6 )
+        self.addLink( s4, h7 )
+        self.addLink( s4, h8 )
+        self.addLink( s5, h9 )
+        self.addLink( s5, h10 )
+        self.addLink( s6, h11 )
+        self.addLink( s6, h12 )
 
-topos = { 'mesh': ( lambda: MyMeshTopo() ) }
+topos = { 'ring': ( lambda: MyTopo() ) }
